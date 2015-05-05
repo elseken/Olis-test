6260b61980581b684ee4809cf4c5a2b2204e54be
elseken <noreply>
2015-05-05 20:56:16 +0000

    Add component task.real

---
+++ task.real.reel/task.real.js
@@ -0,0 +1,16 @@
+/**
+ * @module //task.real.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Task.real
+ * @extends Component
+ */
+exports.Task.real = Component.specialize(/** @lends Task.real# */ {
+    constructor: {
+        value: function Task.real() {
+            this.super();
+        }
+    }
+});
