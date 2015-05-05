f298c85628ffae59fdbf84bfe73a699449dc7476
elseken <noreply>
2015-05-05 20:41:31 +0000

    Add component myfirst

---
+++ ui/myfirst.reel/myfirst.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/myfirst.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Myfirst
+ * @extends Component
+ */
+exports.Myfirst = Component.specialize(/** @lends Myfirst# */ {
+    constructor: {
+        value: function Myfirst() {
+            this.super();
+        }
+    }
+});
